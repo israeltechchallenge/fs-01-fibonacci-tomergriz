74c713342d9bbe3aaf246656659ade11f7669b3d
tomergriz <34768075+tomergriz@users.noreply.github.com>
2022-06-20 19:54:18 +0300

    Milestone 3 Fibonacci - Tomer Grizitzky

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 function Fibonacci(x) {
 
     let i;
-    let fib = [0, 1]; 
+    let fib = [0, 1];
 
     for (i = 2; i <= x; i++) {
-
         fib[i] = fib[i - 2] + fib[i - 1];
     }
-
     return fib[x];
 }
 
-const x = 6;
-const y = Fibonacci(x);
+document.getElementById("button").addEventListener("click", function() {
 
-document.getElementById("Fibonacci").innerText = `The Fibonacci of ${x} is ${y}`;
+var x = document.getElementById("inputNum").value;
+    document.getElementById("result").innerHTML = Fibonacci(x);
+
+})
